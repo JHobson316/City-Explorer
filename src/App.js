46056291f5eba9b31a78ee941ea2a60c97aa615a
Jordan Hobson <hobsoncjordan@gmail.com>
2022-08-28 14:06:55 -0500

    React Repo and keys

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  handleClick =  () => {
+    let API = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION}`;
+    let res = await axios.get(API);
+    res.send(res.data);
+  }
   render() {
     return (
       <div>
