eba9615d4261e0edf7184da6c806faceeea511d9
Jordan Hobson <hobsoncjordan@gmail.com>
2022-08-28 16:52:23 -0500

    Added API

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   handleClick =  () => {
-    let API = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION}`;
+    let API = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION}&q=`;
     let res = await axios.get(API);
+    console.log("Bop");
     res.send(res.data);
   }
   render() {
     return (
       <div>
         <h2>Yo</h2>
+        <button onClick={this.handleClick}>Explore!</button>
       </div>
     )
   }
